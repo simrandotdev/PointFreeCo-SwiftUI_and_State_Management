6b9dfd9634a17b72a29470df2a2e5cfe5b25463c
Simran Preet Singh <simran5590@gmail.com>
2022-08-17 08:34:07 -0700

    Initial Commit

---
+++ PointFreeCo-SwiftUI_and_State_Management/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PointFreeCo-SwiftUI_and_State_Management
+//
+//  Created by Simran Preet Narang on 2022-08-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PointFreeCo-SwiftUI_and_State_Management/PointFreeCo_SwiftUI_and_State_ManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  PointFreeCo_SwiftUI_and_State_ManagementApp.swift
+//  PointFreeCo-SwiftUI_and_State_Management
+//
+//  Created by Simran Preet Narang on 2022-08-17.
+//
+
+import SwiftUI
+
+@main
+struct PointFreeCo_SwiftUI_and_State_ManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
